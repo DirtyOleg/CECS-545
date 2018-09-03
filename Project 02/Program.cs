175571444b429457b5da3763d631d502a6d15242
DirtyOleg <11252872+DirtyOleg@users.noreply.github.com>
2018-09-03 17:53:25 -0400

    Project 02 Draft

--- Project 02/Program.cs
+++ Project 02/Program.cs
@@ -9,27 +9,31 @@ namespace Project_02
 {
     class Program
     {
-        static CityInfo[] cities;
-        static double shortestDistance;
+        static CityInfo[] cities = new CityInfo[11];
         static Queue<CityInfo> queue = new Queue<CityInfo>();
         static Stack<CityInfo> stack = new Stack<CityInfo>();
 
         static void Main(string[] args)
         {
-            //BFS way 
             InitializeAllCityInfo();
+
+            //BFS way
+            Console.WriteLine("the result for using BFS algorithm:");
+            ResetDistanceAndPath();
             BFSMethod();
 
+            Console.WriteLine();
+
             //DFS way
-            InitializeAllCityInfo();
+            Console.WriteLine("the result for using DFS alogithm:");
+            ResetDistanceAndPath();
             DFSMethod();
+
+            Console.Read();
         }
 
         static void InitializeAllCityInfo()
         {
-            cities = new CityInfo[11];
-            shortestDistance = double.MaxValue;
-
             //read all lines from the .tsp file, each line will be one element in the following array
             //File.ReadAllLines method will ignore the last empty line
             string[] data = File.ReadAllLines("11PointDFSBFS.tsp");
@@ -39,53 +43,40 @@ namespace Project_02
             for (int i = 7; i < data.Length; i++)
             {
                 //split the line by the space
-                string[] temp = data[i].Split(' ');
-                cities[i - 7] = new CityInfo() { ID = i - 6, X = Convert.ToDouble(temp[1]), Y = Convert.ToDouble(temp[2]), isVisited = false };
+                string[] line = data[i].Split(' ');
+                cities[i - 7] = new CityInfo()
+                {
+                    ID = i - 6,
+                    X = Convert.ToDouble(line[1]),
+                    Y = Convert.ToDouble(line[2])
+                };
             }
 
             //manually assign connected cities
-            cities[0].connectedCities = new CityInfo[] {
-                new CityInfo() {ID = 2, isVisited = false},
-                new CityInfo() {ID = 3, isVisited = false},
-                new CityInfo() {ID = 4, isVisited = false}
-            };
-            cities[1].connectedCities = new CityInfo[] {
-                new CityInfo() {ID = 3, isVisited = false}
-            };
-            cities[2].connectedCities = new CityInfo[] {
-                new CityInfo() {ID = 4, isVisited = false},
-                new CityInfo() {ID = 5, isVisited = false}
-            };
-            cities[3].connectedCities = new CityInfo[] {
-                new CityInfo() {ID = 5, isVisited = false},
-                new CityInfo() {ID = 6, isVisited = false},
-                new CityInfo() {ID = 7, isVisited = false}
-            };
-            cities[4].connectedCities = new CityInfo[] {
-                new CityInfo() {ID = 7, isVisited = false},
-                new CityInfo() {ID = 8, isVisited = false}
-            };
-            cities[5].connectedCities = new CityInfo[] {
-                new CityInfo() {ID = 8, isVisited = false}
-            };
-            cities[6].connectedCities = new CityInfo[] {
-                new CityInfo() {ID = 9, isVisited = false},
-                new CityInfo() {ID = 10, isVisited = false}
-            };
-            cities[7].connectedCities = new CityInfo[] {
-                new CityInfo() {ID = 9, isVisited = false},
-                new CityInfo() {ID = 10, isVisited = false},
-                new CityInfo() {ID = 11, isVisited = false}
-            };
-            cities[8].connectedCities = new CityInfo[] {
-                new CityInfo() {ID = 11, isVisited = false}
-            };
-            cities[9].connectedCities = new CityInfo[] {
-                new CityInfo() {ID = 11, isVisited = false}
-            };
-            cities[10].connectedCities = new CityInfo[] {
+            cities[0].connectedCities = new CityInfo[] { cities[1], cities[2], cities[3] };
+            cities[1].connectedCities = new CityInfo[] { cities[2] };
+            cities[2].connectedCities = new CityInfo[] { cities[3], cities[4] };
+            cities[3].connectedCities = new CityInfo[] { cities[4], cities[5], cities[6] };
+            cities[4].connectedCities = new CityInfo[] { cities[6], cities[7] };
+            cities[5].connectedCities = new CityInfo[] { cities[7] };
+            cities[6].connectedCities = new CityInfo[] { cities[8], cities[9] };
+            cities[7].connectedCities = new CityInfo[] { cities[8], cities[9], cities[10] };
+            cities[8].connectedCities = new CityInfo[] { cities[10] };
+            cities[9].connectedCities = new CityInfo[] { cities[10] };
+            cities[10].connectedCities = new CityInfo[] { };
+        }
 
-            };
+        static void ResetDistanceAndPath()
+        {
+            foreach (CityInfo city in cities)
+            {
+                city.distanceFromSource = double.MaxValue;
+                city.pathFromSource = new List<int>();
+            }
+
+            //manually assign the information of the starting city
+            cities[0].distanceFromSource = 0;
+            cities[0].pathFromSource.Add(1);
         }
 
         static double CalculateDistance(CityInfo cityOne, CityInfo cityTwo)
@@ -103,13 +94,66 @@ namespace Project_02
                 foreach (CityInfo connectedCity in dequeuedCity.connectedCities)
                 {
                     queue.Enqueue(connectedCity);
-                }
+                    double distanceFromsource = dequeuedCity.distanceFromSource + CalculateDistance(dequeuedCity, connectedCity);
+
+                    if (distanceFromsource < connectedCity.distanceFromSource)
+                    {
+                        //reset distance and path
+                        connectedCity.distanceFromSource = distanceFromsource;
+
+                        connectedCity.pathFromSource.Clear();
+                        foreach (int cityId in dequeuedCity.pathFromSource)
+                        {
+                            connectedCity.pathFromSource.Add(cityId);
+                        }
+                        connectedCity.pathFromSource.Add(connectedCity.ID);
+                    }
+                }// end foreach
+            }// end while()
+
+            Console.WriteLine("the shortest distance is: " + cities[10].distanceFromSource);
+            Console.Write("the shortest path is: ");
+            foreach (int cityId in cities[10].pathFromSource)
+            {
+                Console.Write(cityId + " ");
             }
+            Console.WriteLine();
         }
 
         static void DFSMethod()
         {
+            stack.Push(cities[0]);
 
+            while (stack.Count != 0)
+            {
+                CityInfo popCity = stack.Pop();
+                foreach (CityInfo connectedCity in popCity.connectedCities)
+                {
+                    stack.Push(connectedCity);
+                    double distanceFromsource = popCity.distanceFromSource + CalculateDistance(popCity, connectedCity);
+
+                    if (distanceFromsource < connectedCity.distanceFromSource)
+                    {
+                        //reset distance and path
+                        connectedCity.distanceFromSource = distanceFromsource;
+
+                        connectedCity.pathFromSource.Clear();
+                        foreach (int cityId in popCity.pathFromSource)
+                        {
+                            connectedCity.pathFromSource.Add(cityId);
+                        }
+                        connectedCity.pathFromSource.Add(connectedCity.ID);
+                    }
+                }// end foreach
+            }// end while
+
+            Console.WriteLine("the shortest distance is: " + cities[10].distanceFromSource);
+            Console.Write("the shortest path is: ");
+            foreach (int cityId in cities[10].pathFromSource)
+            {
+                Console.Write(cityId + " ");
+            }
+            Console.WriteLine();
         }
     }
 
@@ -119,17 +163,8 @@ namespace Project_02
         public int ID { get; set; }
         public double X { get; set; }
         public double Y { get; set; }
-        public bool isVisited { get; set; }
-        public CityInfo[] connectedCities { get; set; }
-
-        ////used for deep copy : ICloneable
-        //public object Clone()
-        //{
-        //    CityInfo newOne = new CityInfo();
-        //    newOne.ID = this.ID;
-        //    newOne.X = this.X;
-        //    newOne.Y = this.Y;
-        //    return newOne;
-        //}
+        public CityInfo[] connectedCities { get; set; } //this one is manually assigned
+        public double distanceFromSource { get; set; }
+        public List<int> pathFromSource { get; set; }
     }
 }
